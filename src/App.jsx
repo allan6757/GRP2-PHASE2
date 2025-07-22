70f1b8843b92b3df5b702193ac3198799c88c0dd
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-07-22 15:43:54 +0300

    cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import home from './components/Home';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import Home from './components/Home';
 import Navbar from './components/Navbar';
-import items from './components/Items';
+import Items from './components/Items';
 import Cart from './components/Cart';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
-     <Navbar />
-     <home />
-     <items />
-     <Cart />
-
+      <Navbar />
+      <Home />
+      <Items />
+      <Cart />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
