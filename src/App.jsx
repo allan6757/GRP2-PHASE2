4e5f1ecc886672807311e3ac3468147fd47a4318
muzna <ebrahimmuznah98@gmail.com>
2025-07-22 12:07:19 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import home from './components/home'
+import Navbar from './components/Navbar'
+import items from './components/items'
 
 function App() {
   const [count, setCount] = useState(0)
