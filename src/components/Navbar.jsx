18b6a4f7c02f877f866398735af5e03d0aa20231
allan6757 <allanoomaina@gmail.com>
2025-07-22 11:59:18 +0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+// components/NavBar.jsx
+
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+// Functional component for navigation bar
+function NavBar() {
+    return (
+        <>
+            {/* React fragment wraps everything without adding extra DOM element */}
+            <nav className="navbar">
+                {/* Logo section that links to Home */}
+                <div className="logo">
+                    <Link to="/">SneakerStore</Link>
+                </div>
+
+                {/* Navigation menu */}
+                <ul className="nav-links">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/cart">Cart</Link></li>
+                </ul>
+                {/* Search bar */}
+                <div className="search-bar">
+                    <input type="text" placeholder="Search sneakers..." />
+                    <button>Search</button>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default NavBar;
